26b263c2bd932a3fc98e3e4293658ed0bdd76d1e
Mike Jackson <10079986+llmikeyj@users.noreply.github.com>
2024-03-05 10:59:23 +0000

    Hipp 1016 - deploy api endpoint (#101)
    * HIPP-1016: Test controller for deployments api - wip
    
    * HIPP-1016: Deploy API Controller

---
+++ app/uk/gov/hmrc/apihubapplications/controllers/DeployApiController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apihubapplications.controllers
+
+import com.google.inject.{Inject, Singleton}
+import play.api.Logging
+import play.api.libs.json.{JsError, JsSuccess, JsValue}
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.apihubapplications.connectors.SimpleApiDeploymentConnectorImpl
+import uk.gov.hmrc.apihubapplications.controllers.actions.IdentifierAction
+import uk.gov.hmrc.apihubapplications.models.exception.SimpleApiDeploymentException
+import uk.gov.hmrc.apihubapplications.models.exception.SimpleApiDeploymentException.InvalidResponse
+import uk.gov.hmrc.apihubapplications.models.simpleapideployment.{GenerateRequest, InvalidOasResponse}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeployApiController @Inject()(identify: IdentifierAction,
+                                    cc: ControllerComponents,
+                                    simpleApiDeploymentConnector: SimpleApiDeploymentConnectorImpl)(implicit ec: ExecutionContext)
+  extends BackendController(cc) with Logging {
+
+  def generate: Action[JsValue] = identify.compose(Action(parse.json)).async {
+    implicit request =>
+      val jsReq = request.body
+      jsReq.validate[GenerateRequest] match {
+        case JsSuccess(generateRequest, _) => simpleApiDeploymentConnector.generate(generateRequest) map {
+          case Right(InvalidOasResponse(_)) => BadRequest
+          case Right(_) => Accepted
+          case Left(e: SimpleApiDeploymentException) if e.issue equals InvalidResponse => BadRequest
+          case Left(_) => InternalServerError
+        }
+        case e: JsError =>
+          logger.warn(s"Error parsing request body: ${JsError.toJson(e)}")
+          Future.successful(BadRequest)
+      }
+  }
+}
