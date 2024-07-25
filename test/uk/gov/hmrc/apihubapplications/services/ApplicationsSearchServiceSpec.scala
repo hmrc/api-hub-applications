f926c7549711d6511ee9a7e06917589d050c3a25
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-07-25 15:32:10 +0100

    ApplicationsSearchServiceSpec missing tests

--- test/uk/gov/hmrc/apihubapplications/services/ApplicationsSearchServiceSpec.scala
+++ test/uk/gov/hmrc/apihubapplications/services/ApplicationsSearchServiceSpec.scala
@@ -20,9 +20,13 @@ import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.must.Matchers
 import uk.gov.hmrc.apihubapplications.connectors.IdmsConnector
-import uk.gov.hmrc.apihubapplications.models.application.{Application, Creator, Deleted, TeamMember}
+import uk.gov.hmrc.apihubapplications.models.application.{Api, Application, Creator, Credential, Deleted, Primary, Scope, Secondary, TeamMember}
 import uk.gov.hmrc.apihubapplications.models.application.ApplicationLenses._
+import uk.gov.hmrc.apihubapplications.models.exception.{ApplicationNotFoundException, IdmsException}
+import uk.gov.hmrc.apihubapplications.models.exception.IdmsException.CallError
+import uk.gov.hmrc.apihubapplications.models.idms.{ClientResponse, ClientScope}
 import uk.gov.hmrc.apihubapplications.repositories.ApplicationsRepository
+import uk.gov.hmrc.http.HeaderCarrier
 
 import java.time.{Clock, Instant, LocalDateTime, ZoneId}
 import scala.concurrent.Future
@@ -87,6 +91,151 @@ class ApplicationsSearchServiceSpec extends AsyncFreeSpec with Matchers with Moc
         actual =>
           actual mustBe applications
           verify(repository).findAll(eqTo(None), eqTo(true))
+          succeed
+      }
+    }
+  }
+
+  "findAllUsingApi" - {
+    "must return all applications from the repository that have a given API and are not deleted" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val apiId = "test-api"
+      val applications = Seq(
+        Application(Some("test-id-1"), "test-name-1", Creator("test-email-1"), Seq.empty).addApi(Api(apiId)),
+        Application(Some("test-id-1"), "test-name-1", Creator("test-email-1"), Seq.empty).addApi(Api(apiId)),
+      )
+
+      when(repository.findAllUsingApi(any, any)).thenReturn(Future.successful(applications))
+
+      service.findAllUsingApi(apiId, false) map {
+        actual =>
+          actual mustBe applications
+          verify(repository).findAllUsingApi(eqTo(apiId), eqTo(false))
+          succeed
+      }
+    }
+
+    "must return deleted applications when requested" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val deleted = Deleted(LocalDateTime.now(clock), "test-deleted-by")
+      val apiId = "test-api"
+
+      val applications = Seq(
+        Application(Some("test-id-1"), "test-name-1", Creator("test-email-1"), Seq.empty).addApi(Api(apiId)).delete(deleted),
+        Application(Some("test-id-2"), "test-name-2", Creator("test-email-2"), Seq.empty).addApi(Api(apiId)).delete(deleted)
+      )
+
+      when(repository.findAllUsingApi(any, any)).thenReturn(Future.successful(applications))
+
+      service.findAllUsingApi(apiId, true) map {
+        actual =>
+          actual mustBe applications
+          verify(repository).findAllUsingApi(eqTo(apiId), eqTo(true))
+          succeed
+      }
+    }
+  }
+
+  "findById" - {
+    "must return the application when it exists" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val id = "test-id"
+      val primaryClientId = "test-primary-client-id"
+      val secondaryClientId = "test-secondary-client-id"
+      val secondaryClientSecret = "test-secondary-secret-1234"
+      val scope1 = "test-scope-1"
+      val scope2 = "test-scope-2"
+      val scope3 = "test-scope-3"
+      val scope4 = "test-scope-4"
+
+      val application = Application(Some(id), "test-name", Creator("test-creator"), Seq.empty, clock)
+        .setPrimaryCredentials(Seq(Credential(primaryClientId, LocalDateTime.now(clock), None, None)))
+        .setSecondaryCredentials(Seq(Credential(secondaryClientId, LocalDateTime.now(clock), None, None)))
+
+      when(repository.findById(eqTo(id)))
+        .thenReturn(Future.successful(Right(application)))
+
+      when(idmsConnector.fetchClient(eqTo(Secondary), eqTo(secondaryClientId))(any))
+        .thenReturn(Future.successful(Right(ClientResponse(secondaryClientId, secondaryClientSecret))))
+      when(idmsConnector.fetchClientScopes(eqTo(Secondary), eqTo(secondaryClientId))(any))
+        .thenReturn(Future.successful(Right(Seq(ClientScope(scope1), ClientScope(scope2)))))
+      when(idmsConnector.fetchClientScopes(eqTo(Primary), eqTo(primaryClientId))(any))
+        .thenReturn(Future.successful(Right(Seq(ClientScope(scope3), ClientScope(scope4)))))
+
+      val expected = application
+        .setSecondaryCredentials(Seq(Credential(secondaryClientId, LocalDateTime.now(clock), Some(secondaryClientSecret), Some("1234"))))
+        .setSecondaryScopes(Seq(Scope(scope1), Scope(scope2)))
+        .setPrimaryScopes(Seq(Scope(scope3), Scope(scope4)))
+
+      service.findById(id, enrich = true)(HeaderCarrier()).map {
+        result =>
+          result mustBe Right(expected)
+      }
+    }
+
+    "must return ApplicationNotFoundException when the application does not exist" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val id = "test-id"
+
+      when(repository.findById(eqTo(id)))
+        .thenReturn(Future.successful(Left(ApplicationNotFoundException.forId(id))))
+
+      service.findById(id, enrich = true)(HeaderCarrier()).map(
+        result =>
+          result mustBe Left(ApplicationNotFoundException.forId(id))
+      )
+    }
+
+    "must not return IdmsException when that is returned from the IDMS connector and return issues instead" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val id = "test-id"
+      val clientId = "test-client-id"
+
+      val application = Application(Some(id), "test-name", Creator("test-creator"), Seq.empty, clock)
+        .setSecondaryCredentials(Seq(Credential(clientId, LocalDateTime.now(clock), None, None)))
+
+      val application1WithIssues = application.copy(issues = Seq("Secondary credential not found. test-message"))
+
+      when(repository.findById(eqTo(id)))
+        .thenReturn(Future.successful(Right(application)))
+
+      when(idmsConnector.fetchClient(eqTo(Secondary), eqTo(clientId))(any))
+        .thenReturn(Future.successful(Left(IdmsException("test-message", CallError))))
+      when(idmsConnector.fetchClientScopes(eqTo(Secondary), eqTo(clientId))(any))
+        .thenReturn(Future.successful(Right(Seq.empty)))
+
+      service.findById(id, enrich = true)(HeaderCarrier()).map {
+        result => result mustBe Right(application1WithIssues)
+      }
+    }
+
+    "must not enrich with IDMS data unless asked to" in {
+      val fixture = buildFixture
+      import fixture._
+
+      val id = "test-id"
+
+      val application = Application(Some(id), "test-name", Creator("test-creator"), Seq.empty, clock)
+        .setPrimaryCredentials(Seq(Credential("test-primary-client-id", LocalDateTime.now(clock), None, None)))
+        .setSecondaryCredentials(Seq(Credential("test-secondary-client-id", LocalDateTime.now(clock), None, None)))
+
+      when(repository.findById(eqTo(id)))
+        .thenReturn(Future.successful(Right(application)))
+
+      service.findById(id, enrich = false)(HeaderCarrier()).map {
+        result =>
+          result mustBe Right(application)
+          verifyZeroInteractions(idmsConnector)
           succeed
       }
     }
