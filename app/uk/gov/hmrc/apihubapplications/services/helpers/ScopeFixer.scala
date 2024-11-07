11b4cb64a5954b2e50c2262507aa122a9b2084be
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-11-07 11:42:14 +0000

    Refactor to for comprehension

--- app/uk/gov/hmrc/apihubapplications/services/helpers/ScopeFixer.scala
+++ app/uk/gov/hmrc/apihubapplications/services/helpers/ScopeFixer.scala
@@ -16,14 +16,15 @@
 
 package uk.gov.hmrc.apihubapplications.services.helpers
 
+import cats.data.EitherT
 import com.google.inject.{Inject, Singleton}
 import uk.gov.hmrc.apihubapplications.connectors.{IdmsConnector, IntegrationCatalogueConnector}
 import uk.gov.hmrc.apihubapplications.models.accessRequest.{AccessRequest, Approved}
 import uk.gov.hmrc.apihubapplications.models.api.ApiDetail
 import uk.gov.hmrc.apihubapplications.models.api.ApiDetailLenses.*
-import uk.gov.hmrc.apihubapplications.models.application.{Application, CredentialScopes, EnvironmentName, Primary, Secondary}
 import uk.gov.hmrc.apihubapplications.models.application.ApplicationLenses.*
-import uk.gov.hmrc.apihubapplications.models.exception.{ApiNotFoundException, ApplicationsException, IdmsException}
+import uk.gov.hmrc.apihubapplications.models.application.*
+import uk.gov.hmrc.apihubapplications.models.exception.{ApiNotFoundException, ApplicationsException}
 import uk.gov.hmrc.apihubapplications.services.AccessRequestsService
 import uk.gov.hmrc.apihubapplications.services.helpers.Helpers.{useFirstApplicationsException, useFirstException}
 import uk.gov.hmrc.http.HeaderCarrier
@@ -39,27 +40,15 @@ class ScopeFixer @Inject()(
 
   // Manipulate the model first, adding and removing APIs and endpoints, THEN call this method to fix scopes
   def fix(application: Application)(implicit hc: HeaderCarrier): Future[Either[ApplicationsException, Application]] = {
-    requiredScopes(application).flatMap {
-      case Right(requiredScopes) =>
-        allCredentials(application).flatMap {
-          case Right(credentials) =>
-            accessRequestsService.getAccessRequests(application.id, Some(Approved)).flatMap {
-              accessRequests =>
-                minimiseScopesInEnvironment(Primary, application, credentials, requiredScopes).flatMap {
-                  case Right(application) => minimiseScopesInEnvironment(Secondary, application, credentials, requiredScopes).flatMap {
-                    case Right(application) => addScopesToEnvironment(Secondary, application, requiredScopes).flatMap {
-                      case Right(application) => addScopesToEnvironment(Primary, application, allowedScopes(requiredScopes, accessRequests))
-                      case Left(e) => Future.successful(Left(e))
-                    }
-                    case Left(e) => Future.successful(Left(e))
-                  }
-                  case Left(e) => Future.successful(Left(e))
-                }
-            }
-          case Left(e) => Future.successful(Left(e))
-        }
-      case Left(e) => Future.successful(Left(e))
-    }
+    (for {
+      requiredScopes <- EitherT(requiredScopes(application))
+      credentials <- EitherT(allCredentials(application))
+      accessRequests <- EitherT.right(accessRequestsService.getAccessRequests(application.id, Some(Approved)))
+      application <- EitherT(minimiseScopesInEnvironment(Primary, application, credentials, requiredScopes))
+      application <- EitherT(minimiseScopesInEnvironment(Secondary, application, credentials, requiredScopes))
+      application <- EitherT(addScopesToEnvironment(Primary, application, allowedScopes(requiredScopes, accessRequests)))
+      application <- EitherT(addScopesToEnvironment(Secondary, application, requiredScopes))
+    } yield application).value
   }
 
   private def requiredScopes(application: Application)(implicit hc: HeaderCarrier): Future[Either[ApplicationsException, Set[String]]] = {
@@ -88,7 +77,7 @@ class ScopeFixer @Inject()(
       .intersect(requiredScopes)
   }
 
-  private def allCredentials(application: Application)(implicit hc: HeaderCarrier): Future[Either[IdmsException, Seq[CredentialScopes]]] = {
+  private def allCredentials(application: Application)(implicit hc: HeaderCarrier): Future[Either[ApplicationsException, Seq[CredentialScopes]]] = {
     Future.sequence(
       Seq(Primary, Secondary).flatMap(
         environmentName =>
@@ -114,7 +103,7 @@ class ScopeFixer @Inject()(
     application: Application,
     credentials: Seq[CredentialScopes],
     requiredScopes: Set[String]
-  )(implicit hc: HeaderCarrier): Future[Either[IdmsException, Application]] = {
+  )(implicit hc: HeaderCarrier): Future[Either[ApplicationsException, Application]] = {
     ApplicationEnrichers.process(
       application,
       credentials
@@ -133,7 +122,7 @@ class ScopeFixer @Inject()(
     environmentName: EnvironmentName,
     application: Application,
     allowedScopes: Set[String]
-  )(implicit hc: HeaderCarrier): Future[Either[IdmsException, Application]] = {
+  )(implicit hc: HeaderCarrier): Future[Either[ApplicationsException, Application]] = {
     ApplicationEnrichers.process(
       application,
       allowedScopes
