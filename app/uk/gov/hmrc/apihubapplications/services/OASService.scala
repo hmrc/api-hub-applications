ca25a7b4112a8231f4f642bc930424dbb3777eb5
Mike Jackson <10079986+llmikeyj@users.noreply.github.com>
2024-12-16 13:25:16 +0000

    HIPP-1764: Validate oas title (#224)
    * HIPP-1764: Validate oas title
    
    * HIPP-1764: Validate oas title
    
    * HIPP-1764: Validate oas title
    
    * HIPP-1764: Review actions
    
    * HIPP-1764: Review actions

--- app/uk/gov/hmrc/apihubapplications/services/OASService.scala
+++ app/uk/gov/hmrc/apihubapplications/services/OASService.scala
@@ -16,15 +16,53 @@
 
 package uk.gov.hmrc.apihubapplications.services
 import com.google.inject.{Inject, Singleton}
+import io.circe.{yaml}
 import uk.gov.hmrc.apihubapplications.connectors.APIMConnector
-import uk.gov.hmrc.apihubapplications.models.apim._
+import uk.gov.hmrc.apihubapplications.models.apim.{Error as ApimError, *}
 import uk.gov.hmrc.apihubapplications.models.exception.ApimException
 import uk.gov.hmrc.http.HeaderCarrier
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class OASService @Inject()(apimConnector: APIMConnector) {
-  def validateInPrimary(oas: String)(implicit hc: HeaderCarrier): Future[Either[ApimException, ValidateResponse]] =
-    apimConnector.validateInPrimary(oas)
+
+  val oasTitleError = ApimError("APIM", "Oas title is too long.")
+  val oasTitleMaxSize = 46
+
+  def validateInPrimary(oas: String, validateTitle: Boolean)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Either[ApimException, ValidateResponse]] = {
+
+    if (validateTitle) {
+      val validOasTitle = isValidOasTitle(oas)
+      apimConnector.validateInPrimary(oas) map {
+        case Right(SuccessfulValidateResponse) if validOasTitle => Right(SuccessfulValidateResponse)
+        case Right(SuccessfulValidateResponse) => Right(InvalidOasResponse(newFailuresResponse))
+        case Right(invalidOasResponse: InvalidOasResponse) if validOasTitle => Right(invalidOasResponse)
+        case Right(invalidOasResponse: InvalidOasResponse) =>
+          val failure = invalidOasResponse.failure
+          Right(invalidOasResponse.copy(
+            failure = failure.copy(
+              errors = Some(failure.errors.toList.flatten ++ Seq(oasTitleError)))))
+        case Left(exception) => Left(exception)
+      }
+    } else {
+      apimConnector.validateInPrimary(oas)
+    }
+  }
+
+  private def newFailuresResponse = {
+    FailuresResponse("BAD_REQUEST", s"oas title is longer than $oasTitleMaxSize characters", Some(Seq(oasTitleError)))
+  }
+
+  private def isValidOasTitle(oas: String) = oasTitle(oas).forall(title => {
+    title.length <= oasTitleMaxSize
+  })
+
+  private def oasTitle(oas: String): Option[String] = {
+    val oasYamlOrFail = yaml.parser.parse(oas)
+    oasYamlOrFail.toOption.flatMap(
+      _.findAllByKey("title").headOption.map(_.toString)
+    )
+  }
+
 }
--- test/uk/gov/hmrc/apihubapplications/services/OASServiceSpec.scala
+++ test/uk/gov/hmrc/apihubapplications/services/OASServiceSpec.scala
@@ -16,18 +16,20 @@
 
 package uk.gov.hmrc.apihubapplications.services
 
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, post, stubFor, urlEqualTo}
 import org.mockito.ArgumentMatchers.{any, eq as eqTo}
 import org.mockito.Mockito.when
 import org.scalatest.EitherValues
 import org.scalatest.freespec.AsyncFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.mockito.MockitoSugar
+import play.api.libs.json.Json
 import uk.gov.hmrc.apihubapplications.connectors.APIMConnector
-import uk.gov.hmrc.apihubapplications.models.apim.SuccessfulValidateResponse
+import uk.gov.hmrc.apihubapplications.models.apim.{FailuresResponse, InvalidOasResponse, SuccessfulValidateResponse, Error as ApimError}
 import uk.gov.hmrc.apihubapplications.testhelpers.ApiDetailGenerators
 import uk.gov.hmrc.http.HeaderCarrier
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class OASServiceSpec
   extends AsyncFreeSpec
@@ -36,20 +38,54 @@ class OASServiceSpec
     with ApiDetailGenerators
     with EitherValues {
 
+  implicit override val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+
   "validateInPrimary" - {
-    "must return a validation result" in {
+    "must return a validation result and not validate the title" in {
+
       val fixture = buildFixture()
+      val oas = "some invalid test oas without a title"
 
-      val oas = "valid oas content"
       val validResponse = SuccessfulValidateResponse
-
       when(fixture.apimConnector.validateInPrimary(eqTo(oas))(any)).thenReturn(Future.successful(Right(validResponse)))
 
-      fixture.oasService.validateInPrimary(oas)(HeaderCarrier()).map {
+      fixture.oasService.validateInPrimary(oas, false)(HeaderCarrier()).map {
         result =>
           result.value mustBe validResponse
       }
     }
+
+    "must return a validation result for a valid oas but with title validation error when title too long" in {
+      val fixture = buildFixture()
+
+      val oas = "title: valid oas content but with a title that is much longer than the maximum forty six characters"
+      val validResponse = SuccessfulValidateResponse
+      val invalidResponse = InvalidOasResponse(FailuresResponse("BAD_REQUEST","oas title is longer than 46 characters", Some(Seq(ApimError("APIM", "Oas title is too long.")))))
+
+      when(fixture.apimConnector.validateInPrimary(eqTo(oas))(any)).thenReturn(Future.successful(Right(validResponse)))
+
+      fixture.oasService.validateInPrimary(oas, true)(HeaderCarrier()).map {
+        result =>
+          result.value mustBe invalidResponse
+      }
+    }
+
+    "must return a validation fail result for an invalid oas but with title validation error appended when title too long" in {
+      val fixture = buildFixture()
+
+      val oas = "title: valid oas content but with a title that is much longer than the maximum forty six characters"
+      val apimErrors = Seq(ApimError("jam", "scones"))
+      val invalidOasResponse = InvalidOasResponse(FailuresResponse("cheese", "crackers", Some(apimErrors)))
+      val invalidOasAndTitleResponse = InvalidOasResponse(invalidOasResponse.failure.copy(errors = Some(apimErrors ++ Seq(ApimError("APIM", "Oas title is too long.")))))
+
+      when(fixture.apimConnector.validateInPrimary(eqTo(oas))(any)).thenReturn(Future.successful(Right(invalidOasResponse)))
+
+      fixture.oasService.validateInPrimary(oas, true)(HeaderCarrier()).map {
+        result =>
+          result.value mustBe invalidOasAndTitleResponse
+      }
+    }
+
   }
 
   private case class Fixture(apimConnector: APIMConnector, oasService: OASService)
