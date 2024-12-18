e12ac542303edb0b6a16dff533a0575b20a71b53
Mike Jackson <10079986+llmikeyj@users.noreply.github.com>
2024-12-18 11:24:48 +0000

    Hipp 1764 bug fix (#227)
    * HIPP-1764: Fix yaml string bug
    
    * HIPP-1764: Fix oas parsing
    
    * HIPP-1764: Tidy up
    
    * HIPP-1764: Tidy up
    
    * HIPP-1764: Tidy up
    
    * HIPP-1764: Tidy up
    
    * HIPP-1764: Tidy up

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,9 +14,16 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-metrix-play-30"    % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "internal-auth-client-play-30" % internalAuthVersion,
     "uk.gov.hmrc"             %% "crypto-json-play-30"          % "8.1.0",
-    "io.circe"                %% "circe-yaml"                   % "0.14.2",
-    "io.circe"                %% "circe-generic"                % "0.14.6",
-    "io.circe"                %% "circe-parser"                 % "0.14.6"
+    "io.swagger.parser.v3" % "swagger-parser" % "2.1.22"
+      excludeAll(
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
+      ExclusionRule("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
+    ),
+    "io.swagger" % "swagger-inflector" % "2.0.12"
+      excludeAll(
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
+      ExclusionRule("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
+    ),
   )
 
   val test = Seq(
